deba1d8d98ae052a513ab7777a4fee110fa09296
Malahat Pouransafar <p.malahat@yahoo.com>
2024-01-30 16:40:07 +0100

    create booking tools feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_111553) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_143832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_111553) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "starting_date", precision: nil
+    t.datetime "ending_date", precision: nil
     t.index ["tool_id"], name: "index_bookings_on_tool_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
