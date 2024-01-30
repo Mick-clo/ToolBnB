9ff38d05e85f49704942db5e88c48cfee6279114
michele comande <comande.michele@gmail.com>
2024-01-30 15:34:45 +0000

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@
 #   end
 # Create Tool
 
-User.create!(email: "mick@wagon.com", encrypted_password: "password", password: "password")
 
-Tool.create(title: 'first tool', price: 11, description: 'A very useful tool', user_id: 1)
-Tool.create(title: 'second tool', price: 11, description: 'A very useful tool', user_id: 1)
-Tool.create(title: 'third tool', price: 11, description: 'A very useful tool', user_id: 1)
-Tool.create(title: 'fourth tool', price: 11, description: 'A very useful tool', user_id: 1)
-Tool.create(title: 'fifth tool', price: 11, description: 'A very useful tool', user_id: 1)
-Tool.create(title: 'sixth tool', price: 11, description: 'A very useful tool', user_id: 1)
+
+Tool.create(title: 'Drill', price: 14, description: 'Simple and quick 👌', user_id: 1)
+Tool.create(title: 'Saw', price: 25, description: 'Cuts everything!', user_id: 1)
+Tool.create(title: 'Ladder', price: 8, description: 'I know what it means to not have enough space for a ladder at home', user_id: 1)
+Tool.create(title: 'Hammer', price: 11, description: "When you need it, it's never there", user_id: 1)
+Tool.create(title: 'Axe', price: 17, description: 'Experience the life of a woodcutter for one hour', user_id: 1)
+Tool.create(title: 'Paint Roller', price: 1, description: 'Need soemthing to paint your house?', user_id: 1)
