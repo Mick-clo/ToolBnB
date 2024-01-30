e49523dddeef188060b8b5ed6935ce20e7e8bf55
Olga Gavrysh <olgagavrysh8@gmail.com>
2024-01-30 12:57:37 +0100

    addign a new post

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,2 +1,21 @@
 class ToolsController < ApplicationController
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to @tool, notice: 'Your tool was successfully added!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:title, :price, :description)
+  end
 end
