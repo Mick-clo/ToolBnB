1b8c1f2db8a2e21f20e668ae79e9c2b0f260a58d
Olga Gavrysh <olgagavrysh8@gmail.com>
2024-01-30 14:48:48 +0100

    delete action

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,5 +1,4 @@
 class ToolsController < ApplicationController
-
   before_action :set_tool, only: [:show]
 
   def show
@@ -11,12 +10,12 @@ class ToolsController < ApplicationController
 
   def create
     @tool = Tool.new(tool_params)
+    @tool.user = current_user
     if @tool.save
-      redirect_to @tool, notice: 'Your tool was successfully added!'
+      redirect_to @tool, notice: 'Your tool was successfully added!' # tool_path(@tool)
     else
       render :new
     end
-
   end
 
   def destroy
@@ -25,15 +24,26 @@ class ToolsController < ApplicationController
     redirect_to tools_path, status: :see_other
   end
 
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+  def update
+    @tool = Tool.find(params[:id])
+    if @tool.update(tool_params)
+      redirect_to @tool, notice: 'Tool was successfully updated!'
+    else
+      render :edit
+    end
+  end
 
   private
-
 
   def set_tool
     @tool = Tool.find(params[:id])
   end
+
   def tool_params
     params.require(:tool).permit(:title, :price, :description)
-
   end
 end
