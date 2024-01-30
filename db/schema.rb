70f10876b72e59fa4c21040720fad8fe3c12b927
Timeea Samoila <timeeasamoila12@gmail.com>
2024-01-30 15:30:12 +0100

    login 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_111022) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_131951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "tool_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tool_id"], name: "index_bookings_on_tool_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "tools", force: :cascade do |t|
     t.string "title"
@@ -32,9 +41,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_111022) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "tool_id", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["tool_id"], name: "index_users_on_tool_id"
   end
 
+  add_foreign_key "bookings", "tools"
+  add_foreign_key "bookings", "users"
   add_foreign_key "tools", "users"
+  add_foreign_key "users", "tools"
 end
