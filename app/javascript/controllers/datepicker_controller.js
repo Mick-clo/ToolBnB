898b63c657a1b7db1e93487f20a98450e5e7f36a
michele comande <comande.michele@gmail.com>
2024-02-02 14:45:40 +0000

    flatpickr added

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,7 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
 export default class extends Controller {
+  static targets = ["startingDate", "endingDate"]
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.startingDateTarget, {
+      altInput: true,
+      dateFormat: "d.m.Y",
+      altFormat: "F j, Y",
+      minDate: "today"
+    })
+    flatpickr(this.endingDateTarget, {
+      altInput: true,
+      dateFormat: "d.m.Y",
+      altFormat: "F j, Y",
+      minDate: "today",
+      disable: [this.startingDateTarget.value]
+    })
   }
 }
